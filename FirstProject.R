dbae7006782dcc8ab8fdf925b96471fa4ec7e850
SharmaRupali <rups1501@gmail.com>
2019-05-18 16:51:07 +0200

    added cosine sim. all colors: WiP

--- FirstProject.R
+++ FirstProject.R
@@ -1,8 +1,75 @@
 setwd("C:/Users/rupal/Google Drive/Beuth Uni/Semester II/Data Visualization/First Project")
 
 require(data.table)
+require(colorscience)
+require(tcR)
+
 lab_measure <- fread("LabMeasurements-Color-Card.csv", dec = ",")
 master_color_card <- fread("MasterColorCard.csv", dec = ",")
+
+XX <- data.matrix(lab_measure)
+YY <- data.matrix(master_color_card)
+
+
+## distance (deltaE) & similarity (cosine similarity) between color patches
+
+## each color patch on each card of each paper is compared to the respective color patch on the master color card
+## matrix aa (546,64): rows corresponding to the rows from XX (lab_measure) (1 row = 1 color card with 64 patches), 
+## and cols corresponding to the deltaE values for each color patch: 3 cols (XX)(L, a, b) = 1 col (aa) deltaE
+
+### Correct one --
+aa <- matrix(nrow = dim(XX)[1], ncol = 64)
+bb <- matrix(nrow = 8, ncol = 8)
+
+for (n_row in 1:dim(XX)[1]) {
+  for (i in 1:8) {
+    for (j in 1:8) {
+      bb[i,j] <- deltaE2000(c(XX[n_row ,paste("L", i, j, sep="")], XX[n_row ,paste("a", i, j, sep="")], XX[n_row ,paste("b", i, j, sep="")]),
+                            c(YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 9], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 10], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 11]))
+      aa[n_row, ] <- c(t(bb))
+    }
+  }
+}
+
+dist_mat <- cbind(rep(1:13, 42), XX[, c(1,2)], aa)
+cols <- unique(substr(colnames(XX), 2, 3))[c(-1,-2)]
+colnames(dist_mat) <- c("Sheet", "Rows", "Columns", cols)
+### -- Correct one
+
+
+### correct similarity ---
+ll <- matrix(nrow = dim(XX)[1], ncol = 64)
+cc <- matrix(nrow = 8, ncol = 8)
+
+for (n_row in 1:dim(XX)[1]) {
+  for (i in 1:8) {
+    for (j in 1:8) {
+      cc[i,j] <- cosine.similarity(c(XX[n_row ,paste("L", i, j, sep="")], XX[n_row ,paste("a", i, j, sep="")], XX[n_row ,paste("b", i, j, sep="")]),
+                                   c(YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 9], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 10], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 11]), .do.norm = TRUE)
+      ll[n_row, ] <- c(t(cc))
+    }
+  }
+}
+
+simi_mat <- cbind(dist_mat[, 1:3], ll)
+colnames(simi_mat) <- colnames(dist_mat)
+### --- correct similarity
+
+
+# ignore middle blocks at all times 44, 45, 54, 55
+# all colors
+# without corners 11, 18, 81, 88
+# without borders 1st row: 11,12,13-18 1st col: 11,21,31-81 8th row: 81,82,83-88 8th col: 18,28,38-88
+
+# intra sheet inter color
+# plot every sheet pattern
+
+
+
+
+
+
+########## tests distance
 
 # calculate the distances between the colors (Lab), print master and other cards on sheets
 
@@ -86,29 +153,12 @@ for (n_row in 1:dim(XX)[1]) {
 
 
 
-## distance between color patches
-## each color patch on each card of each paper is compared to the respective color patch on the master color card
-## matrix aa (546,64): rows corresponding to the rows from XX (lab_measure) (1 row = 1 color card with 64 patches), 
-## and cols corresponding to the deltaE values for each color patch: 3 cols (XX)(L, a, b) = 1 col (aa) deltaE
-
-### Correct one --
-aa <- matrix(nrow = dim(XX)[1], ncol = 64)
-bb <- matrix(nrow = 8, ncol = 8)
-
-for (n_row in 1:dim(XX)[1]) {
-  for (i in 1:8) {
-    for (j in 1:8) {
-      bb[i,j] <- deltaE2000(c(XX[n_row ,paste("L", i, j, sep="")], XX[n_row ,paste("a", i, j, sep="")], XX[n_row ,paste("b", i, j, sep="")]),
-                            c(YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 9], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 10], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 11]))
-      aa[n_row, ] <- c(t(bb))
-    }
-  }
-}
-### -- Correct one
+# color(sheet->card) - color(master)
+YY[1, 9:11]
+XX[1:13, 1:5]
 
 
-
-
+## trials (distances)
 aa <- matrix(nrow = dim(XX)[1], ncol = 64)
 bb <- matrix(nrow = dim(XX)[1], ncol = 64)
 
@@ -177,3 +227,53 @@ deltaE2000(c(XX[1,paste("L", 6, 5, sep="")], XX[1,paste("a", 6, 5, sep="")], XX[
 # macter color card: color patches
 require(colorspace)
 LAB(51.641, 0.169, 3.054)
+
+
+
+
+
+
+########## tests similarity
+
+# similarities color(sheet->card) - color(master)
+XX[1, 1:5]
+YY[1, c(2:3, 9:11)]
+
+cosine.similarity(XX[1, 3:5], YY[1, 9:11])
+cosine.similarity(XX[1, 6:8], YY[2, 9:11])
+
+
+cosine.similarity(c(XX[n_row ,paste("L", 1, 1, sep="")], XX[n_row ,paste("a", 1, 1, sep="")], XX[n_row ,paste("b", 1, 1, sep="")]),
+                  c(YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 9], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 10], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 11]), .do.norm = TRUE)
+
+cosine.similarity(c(XX[n_row ,paste("L", 1, 2, sep="")], XX[n_row ,paste("a", 1, 2, sep="")], XX[n_row ,paste("b", 1, 2, sep="")]),
+                  c(YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 2, 9], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 2, 10], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 2, 11]), .do.norm = TRUE)
+
+cosine.similarity(c(XX[n_row ,paste("L", 1, 1, sep="")], XX[n_row ,paste("a", 1, 1, sep="")], XX[n_row ,paste("b", 1, 1, sep="")]),
+                  c(YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 9], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 10], YY[YY[, "Crow"] == 1 & YY[, "Ccol"] == 1, 11]), .do.norm = TRUE)
+
+
+### correct similarity ---
+ll <- matrix(nrow = dim(XX)[1], ncol = 64)
+cc <- matrix(nrow = 8, ncol = 8)
+
+for (n_row in 1:dim(XX)[1]) {
+  for (i in 1:8) {
+    for (j in 1:8) {
+      cc[i,j] <- cosine.similarity(c(XX[n_row ,paste("L", i, j, sep="")], XX[n_row ,paste("a", i, j, sep="")], XX[n_row ,paste("b", i, j, sep="")]),
+                            c(YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 9], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 10], YY[YY[, "Crow"] == i & YY[, "Ccol"] == j, 11]), .do.norm = TRUE)
+      ll[n_row, ] <- c(t(cc))
+    }
+  }
+}
+
+similarity_mat <- cbind(distance_mat[, 1:3], ll)
+colnames(similarity_mat) <- colnames(distance_mat)
+### ---
+
+hist(ll)
+
+
+
+## means
+simi_mat[1, 4:dim(simi_mat)[2]] # for all rows
