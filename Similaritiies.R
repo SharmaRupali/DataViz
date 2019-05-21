a87083b4f5ded35825415249a29f7fe6afc9fe99
SharmaRupali <rups1501@gmail.com>
2019-05-21 18:34:36 +0200

    added boxplot part

--- Similaritiies.R
+++ Similaritiies.R
@@ -27,7 +27,12 @@ for (i in unique(c(paste(1, 1:8, sep=""), paste(8, 1:8, sep=""), paste(1:8, 1, s
 }
 
 
-### correct similarity ---
+## similarity (cosine similarity) between color patches
+
+## each color patch on each card of each paper is compared to the respective color patch on the master color card
+## matrix aa (546,64): rows corresponding to the rows from XX (lab_measure) (1 row = 1 color card with 64 patches), 
+## and cols corresponding to the cosine.similarity values for each color patch: 3 cols (XX)(L, a, b) = 1 col (aa) cosine.similarity
+
 ll <- matrix(nrow = dim(XX)[1], ncol = 64)
 cc <- matrix(nrow = 8, ncol = 8)
 
@@ -64,4 +69,46 @@ for (i in 1:dim(simi_mat)[1]) {
   simi_means[i, 5] <- mean(simi_mat_no_corners[i, 4:dim(simi_mat_no_corners)[2]])
   simi_means[i, 6] <- mean(simi_mat_no_borders[i, 4:dim(simi_mat_no_borders)[2]])
 }
-### --- correct similarity
+
+
+## boxplot similarities all colors
+row_names <- paste(simi_means[which(simi_means[, "Sheet"] == 1), "Rows"], simi_means[which(simi_means[, "Sheet"] == 1), "Columns"], sep = "")
+
+simi_frame_means_all = data.frame()
+for (i in 1:13) {
+  simi_frame_means_all <- rbind(simi_frame_means_all, as.list(simi_means[which(simi_means[, "Sheet"] == i), 4]))
+}
+simi_frame_means_all <- t(simi_frame_means_all)
+row.names(simi_frame_means_all) <- row_names
+write.csv(simi_frame_means_all, "Similarity_frame_means_all.csv")
+
+boxplot(simi_frame_means_all, horizontal = TRUE, main = "Mean similarities wrt cards by sheets", xlab = "Mean similarities wrt cards", ylab = "Sheet")
+abline(v = mean(simi_frame_means_all))
+
+
+## boxplot similarities no corners
+simi_frame_means_no_corners = data.frame()
+for (i in 1:13) {
+  simi_frame_means_no_corners <- rbind(simi_frame_means_no_corners, as.list(simi_means[which(simi_means[, "Sheet"] == i), 5]))
+}
+simi_frame_means_no_corners <- t(simi_frame_means_no_corners)
+row.names(simi_frame_means_no_corners) <- row_names
+write.csv(simi_frame_means_no_corners, "Similarity_frame_means_no_corners.csv")
+
+boxplot(simi_frame_means_no_corners, horizontal = TRUE, main = "Mean similarities wrt cards (no corners) by sheets", xlab = "Mean similarities wrt cards", ylab = "Sheet")
+abline(v = mean(simi_frame_means_no_corners))
+
+
+## boxplot similarities no borders
+simi_frame_means_no_borders = data.frame()
+for (i in 1:13) {
+  simi_frame_means_no_borders <- rbind(simi_frame_means_no_borders, as.list(simi_means[which(simi_means[, "Sheet"] == i), 6]))
+}
+simi_frame_means_no_borders <- t(simi_frame_means_no_borders)
+row.names(simi_frame_means_no_borders) <- row_names
+write.csv(simi_frame_means_no_borders, "Similarity_frame_means_no_borders.csv")
+
+boxplot(simi_frame_means_no_borders, horizontal = TRUE, main = "Mean similarities wrt cards (no borders) by sheets", xlab = "Mean similarities wrt cards", ylab = "Sheet")
+abline(v = mean(simi_frame_means_no_borders))
+
+
