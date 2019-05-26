75a4cbf22d66ec251fde7beb475ce3b8bc6404bc
SharmaRupali <rups1501@gmail.com>
2019-05-26 20:08:38 +0200

    single sheet stats

--- Measurements.R
+++ Measurements.R
@@ -38,6 +38,35 @@ for (i in 1:32){
 }
 
 
+### Sheets
+min_dist_sheet <- c()
+max_dist_sheet <- c()
+mean_dist_sheet <- c()
+median_dist_sheet <- c()
+out_dist_sheet <- list()
+
+for(i in 1:13) {
+  min_dist_sheet <- c(min_dist_sheet, round(min(dist_frame_means_no_borders[,i]), 3))
+  max_dist_sheet <- c(max_dist_sheet, round(max(dist_frame_means_no_borders[,i]), 3))
+  mean_dist_sheet <- c(mean_dist_sheet, round(mean(dist_frame_means_no_borders[,i]), 3))
+  median_dist_sheet <- c(median_dist_sheet, median(dist_frame_means_no_borders[,i]))
+  out_dist_sheet[[i]] <- round(boxplot.stats(dist_frame_means_no_borders[,i])$out, 3)
+}
+
+sheet_dist_means <- cbind(min_dist_sheet, max_dist_sheet, mean_dist_sheet, median_dist_sheet, out_dist_sheet)
+rownames(sheet_dist_means) <- c(1:13)
+colnames(sheet_dist_means) <- c("Min", "Max", "Mean", "Median", "No. Outliers")
+sheet_dist_means
+# write.csv(sheet_dist_means, "sheet_dist_means.csv")
+
+# sheet distance outliers
+for (i in 1:13){
+  print(paste('-', i, '-'))
+  print(length(boxplot.stats(dist_frame_means_no_borders[,i])$out))
+}
+
+
+
 # SIMILARITIES
 
 # cards
@@ -87,3 +116,32 @@ round(min(simi_mat_no_borders[,4:35]), 3)
 round(mean(simi_mat_no_borders[,4:35]),3)
 round(boxplot.stats(simi_mat_no_borders[,4:35])$out,3)
 boxplot.stats(simi_mat_no_borders[,4:35])$out
+
+
+### Sheets
+min_simi_sheet <- c()
+max_simi_sheet <- c()
+mean_simi_sheet <- c()
+median_simi_sheet <- c()
+out_simi_sheet <- list()
+
+for(i in 1:13) {
+  min_simi_sheet <- c(min_simi_sheet, round(min(simi_frame_means_no_borders[,i]), 3))
+  max_simi_sheet <- c(max_simi_sheet, round(max(simi_frame_means_no_borders[,i]), 3))
+  mean_simi_sheet <- c(mean_simi_sheet, round(mean(simi_frame_means_no_borders[,i]), 3))
+  median_simi_sheet <- c(median_simi_sheet, median(simi_frame_means_no_borders[,i]))
+  out_simi_sheet[[i]] <- round(boxplot.stats(simi_frame_means_no_borders[,i])$out, 3)
+}
+
+sheet_simi_means <- cbind(min_simi_sheet, max_simi_sheet, mean_simi_sheet, median_simi_sheet, out_simi_sheet)
+rownames(sheet_simi_means) <- c(1:13)
+colnames(sheet_simi_means) <- c("Min", "Max", "Mean", "Median", "No. Outliers")
+sheet_simi_means
+# write.csv(sheet_simi_means, "sheet_simi_means.csv")
+
+# sheet distance outliers
+for (i in 1:13){
+  print(paste('-', i, '-'))
+  print(length(boxplot.stats(simi_frame_means_no_borders[,i])$out))
+}
+
