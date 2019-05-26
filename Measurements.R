b1cc2769fa1fe849b0088fdd961a06071cb072c0
kwbln <44318228+kwbln@users.noreply.github.com>
2019-05-26 14:39:42 +0200

    current status

---
+++ Measurements.R
@@ -0,0 +1,29 @@
+
+mean_dist_frame_means_no_borders <- round(mean(dist_frame_means_no_borders),3); mean_dist_frame_means_no_borders
+min_dist_frame_means_no_borders <- round(min(dist_frame_means_no_borders),3); min_dist_frame_means_no_borders
+max_dist_frame_means_no_borders <- round(max(dist_frame_means_no_borders),3); max_dist_frame_means_no_borders
+outlier_dist_frame_means_no_borders <- round(boxplot.stats(dist_frame_means_no_borders)$out,3); outlier_dist_frame_means_no_borders
+
+mean_simi_frame_means_no_borders <- round(mean(simi_frame_means_no_borders),3); mean_simi_frame_means_no_borders
+min_simi_frame_means_no_borders <- round(min(simi_frame_means_no_borders),3); min_simi_frame_means_no_borders
+max_simi_frame_means_no_borders <- round(max(simi_frame_means_no_borders),3); max_simi_frame_means_no_borders
+outlier_simi_frame_means_no_borders <- round(boxplot.stats(simi_frame_means_no_borders)$out,3); outlier_simi_frame_means_no_borders
+
+min_dist_col <- c()
+max_dist_col<- c()
+mean_dist_col<- c()
+out_dist_col <- c()
+
+for(i in 4:35) {
+  min_dist_col <- c(min_dist_col,round(min(dist_mat_no_borders[,i]),3))
+  max_dist_col <- c(max_dist_col,round(max(dist_mat_no_borders[,i]),3))
+  mean_dist_col <- c(mean_dist_col,round(mean(dist_mat_no_borders[,i]),3))
+  out_dist_col <-   c(out_dist_col, round(boxplot.stats(dist_mat_no_borders[,i])$out,3))
+}
+min_dist_col
+col_meas <- cbind(min_dist_col, max_dist_col, mean_dist_col)
+col_meas.
+rownames(col_meas) <- c(1:32)
+colnames(col_meas) <- c("Min", "Max", "Mean")
+col_meas
+write.csv(col_meas, file = "Data/col_meas.csv")
